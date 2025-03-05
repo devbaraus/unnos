942b654f504c52baa1017e1608aa95bcddec8ece
Bruno de Araujo Alves <me@baraus.dev>
2025-03-05 13:08:25 -0300

    feat: initial commit

---
+++ src/bot.ts
@@ -0,0 +1,18 @@
+import { env } from 'bun';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(env.TOKEN);
---
+++ src/rest.ts
@@ -0,0 +1,21 @@
+import { REST, Routes } from 'discord.js';
+import { env } from './env'
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(env.TOKEN);
+
+try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(env.CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+    console.error(error);
+}
